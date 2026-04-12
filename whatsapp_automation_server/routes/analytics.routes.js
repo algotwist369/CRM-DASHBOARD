45c073dd3f9e5bb820f9b80d5afd04deb5de264d
ankit pathak <info.algotwist@gmail.com>
2026-04-12 17:52:42 +0530

    removed whatsapp automation

--- whatsapp_automation_server/routes/analytics.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getAnalytics } = require("../controller/analytics.controller");
-
-router.get("/", getAnalytics);
-
-module.exports = router;
-
